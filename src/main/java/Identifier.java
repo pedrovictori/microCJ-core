87d48500493baf8e3342dbcb691b00a5ae7eca30
Pedro Victori <fosforilasa@gmail.com>
2019-02-22 18:20:48 +0000

    Identifiable and Identifier

---
+++ src/main/java/Identifier.java
@@ -0,0 +1,11 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Identifier implements Identifiable {
+    static final AtomicInteger NEXT_ID = new AtomicInteger(0);
+    final int id = NEXT_ID.getAndIncrement(); //get a new id when the new object initializes its fields. Atomic ensures safety in concurrency
+
+    @Override
+    public int getId() {
+        return id;
+    }
+}
