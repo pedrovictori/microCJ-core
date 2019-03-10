33d3396d54c24dce2cd9e3f4916b857fa4c90081
Pedro Victori <fosforilasa@gmail.com>
2019-03-10 03:10:29 +0000

    parsing positive attribute

--- src/main/java/graph/GeneLink.java
+++ src/main/java/graph/GeneLink.java
@@ -6,10 +6,16 @@ import core.Node;
 public class GeneLink extends Identifier {
 	private Node target;
 	private Node source;
+	private boolean positive;
 
 	public GeneLink(Node target, Node source){
 		this.target = target;
 		this.source = source;
+	}
+
+	public GeneLink(Node target, Node source, boolean positive) {
+		this(target, source);
+		this.positive = positive;
 	}
 
 	public Node getTarget() {
@@ -18,6 +24,10 @@ public class GeneLink extends Identifier {
 
 	public Node getSource() {
 		return source;
+	}
+
+	public boolean isPositive() {
+		return positive;
 	}
 
 	@Override
