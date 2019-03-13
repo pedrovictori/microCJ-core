96f2e64d1f330d75d91f80b7ea902adbd5910f24
Pedro Victori <fosforilasa@gmail.com>
2019-03-13 04:52:26 +0000

    changes to Cell

--- src/main/java/core/Cell.java
+++ src/main/java/core/Cell.java
@@ -1,18 +1,63 @@
 package core;
 
+import geom.Point3D;
 import graph.GeneGraph;
 
 public class Cell extends Identifier {
     private boolean alive = true;
     private int age = 0;
     private GeneGraph geneGraph;
+    private Point3D location;
+
+    /**
+     * the cell radius
+     */
+    private double size;
+    private final static double DEFAULT_SIZE = 10;
 
     public Cell() {
         geneGraph = new GeneGraph(getId());
+        size = DEFAULT_SIZE;
+    }
+
+    public Cell(Point3D location) {
+        this();
+        this.location = location;
+    }
+
+    public Cell(Point3D location, double size) {
+        this(location);
+        this.size = size;
+    }
+
+
+
+    public static double getDefaultSize() {
+        return DEFAULT_SIZE;
     }
 
     public boolean isAlive() {
         return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    public Point3D getLocation() {
+        return location;
+    }
+
+    public void setLocation(Point3D location) {
+        this.location = location;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
     }
 
     public int getAge() {
