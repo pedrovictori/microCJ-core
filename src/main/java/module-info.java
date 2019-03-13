4dbadc68086f6781d80bbcb388da59e87e4654c1
Pedro Victori <fosforilasa@gmail.com>
2019-03-13 04:53:40 +0000

    updating exporting of dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module microC2.core {
     exports graph;
-    requires org.jgrapht.core;
+	exports geom;
+	exports core;
+	requires org.jgrapht.core;
     requires org.jgrapht.io;
     requires jbool.expressions;
 }
