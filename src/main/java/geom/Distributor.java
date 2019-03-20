bf032f566e88c21a2c1b782d74e884d677925bcd
Pedro Victori <fosforilasa@gmail.com>
2019-03-20 21:00:52 +0000

    method for placing a new cell

--- src/main/java/geom/Distributor.java
+++ src/main/java/geom/Distributor.java
@@ -1,7 +1,21 @@
 package geom;
+/**
+ * @author Pedro Victori
+ */
+/*
+Copyright 2019 Pedro Victori
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
 
 import java.util.Set;
 
 public interface Distributor {
 	Set<Point3D> populate(int n, double cellRadius);
+
+	Point3D locateEmptySpotNextTo(Point3D location, double distance, Set<Point3D> locations);
 }
