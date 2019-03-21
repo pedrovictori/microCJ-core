1cc7bb489cf2ce215c5c9388b96271ab3cbbb27f
Pedro Victori <fosforilasa@gmail.com>
2019-03-21 17:45:47 +0000

    Complying with interface, needs implementing

--- src/main/java/geom/RegularRandomDistributor.java
+++ src/main/java/geom/RegularRandomDistributor.java
@@ -1,6 +1,18 @@
 package geom;
 
 import java.util.*;
+/**
+ * @author Pedro Victori
+ */
+/*
+Copyright 2019 Pedro Victori
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
 
 public class RegularRandomDistributor implements Distributor{
 	private static Point3D[] regularIntervals;
@@ -60,5 +72,11 @@ public class RegularRandomDistributor implements Distributor{
 			}
 		}
 		return points;
+	}
+
+	@Override
+	public Point3D locateEmptySpotNextTo(Point3D location, double distance, Set<Point3D> locations) {
+		return null;
+		// TODO: 19/03/2019 implement
 	}
 }
