af04964cedbb05a467a8e0674a22624a786a1030
Pedro Victori <fosforilasa@gmail.com>
2019-03-21 17:43:33 +0000

    UpdateFlag enum

---
+++ src/main/java/update/UpdateFlag.java
@@ -0,0 +1,10 @@
+package update;
+
+/**
+ * This enum is made up of flags that are expected to be placed in a queue populated by the model so that the viewer can look into it and update the graphical user interface as needed
+ */
+public enum UpdateFlag{
+	DEAD_CELL,
+	NECROTIC_CELL,
+	NEW_CELL;
+}
