14fe34587a1275584aa031518ed98b6e28a68c47
Pedro Victori <fosforilasa@gmail.com>
2019-04-03 03:01:00 +0100

    DiffusionLattice stub

---
+++ src/main/java/diffusion/DiffusionLattice.java
@@ -0,0 +1,20 @@
+package diffusion;
+
+import java.util.Map;
+
+public class DiffusionLattice {
+	//todo implement
+
+	public void addSubstance(Diffusible substance, Double concentration, int i, int j, int k) {
+		//todo implement
+	}
+
+	public Map<Diffusible, Double> getSubstancesAt(int i, int j, int k) {
+		//todo implement
+		return null;
+	}
+
+	public void update() {
+		//todo implement
+	}
+}
